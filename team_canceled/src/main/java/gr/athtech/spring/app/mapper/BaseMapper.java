e6f5b425f8fcb38d24120cbc1234418b343486d5
charis-ant <harisantoniadi@gmail.com>
2024-01-08 13:15:50 +0200

    Added mapper layer, plugins & properties - getting compilation error (when maven clean --> maven package):  Compilation failure error: release version 17 not supported

---
+++ team_canceled/src/main/java/gr/athtech/spring/app/mapper/BaseMapper.java
@@ -0,0 +1,14 @@
+package gr.athtech.spring.app.mapper;
+
+import java.util.List;
+
+public interface BaseMapper<D, R> {
+    R toResource(D domain);
+
+    List<R> toResource(List<D> domains);
+
+    D toDomain(R resource);
+    List<D> toDomains(List<R> resources);
+
+
+}
