e6817a8821346a927c0c3d32995929251ae187d4
Elemxm <elenamaxmoutai@gmail.com>
2024-01-08 21:58:50 +0200

    added exceptions to AccountServiceImpl

--- team_canceled/src/main/java/gr/athtech/spring/app/service/AccountServiceImpl.java
+++ team_canceled/src/main/java/gr/athtech/spring/app/service/AccountServiceImpl.java
@@ -9,7 +9,9 @@ import gr.athtech.spring.app.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +33,7 @@ public class AccountServiceImpl extends BaseServiceImpl<Account> implements Acco
         return accountRepository.findByPhone(phone);
     }
 
+    //Check this again!
     public boolean signup(Account account) {
         //1) Check through Repository account already exists
         if (accountRepository.exists(account)) {
@@ -55,18 +58,38 @@ public class AccountServiceImpl extends BaseServiceImpl<Account> implements Acco
 
     @Override
     public void changePassword(Account account, String password) {
+        accountRepository.update(account);
+
         if (accountRepository.exists(account)) {
+            if (password == null) {
+                throw new IllegalArgumentException("Password cannot be null");
+            }
+
             account.setPassword(password);
+            accountRepository.update(account);
+        }
+        else {
+            throw new NoSuchElementException("Account not found");
         }
 
-        accountRepository.update(account);
     }
 
     @Override
     public void addAddress(Account account, Address address) {
-        account.getAddresses().add(address);
-        accountRepository.update(account);
+
+        if( address.getStreetName() == null || address.getStreetNumber() == null || address.getPostalCode() == null) {
+            throw new IllegalArgumentException("Address field cannot be null");
+        } else {
+            if (account.getAddresses().contains(address)) {
+                throw new IllegalStateException("Address already exists for the account");
+            } else {
+                account.getAddresses().add(address);
+                accountRepository.update(account);
+            }
+        }
+
     }
+
 
     //pending
     @Override
